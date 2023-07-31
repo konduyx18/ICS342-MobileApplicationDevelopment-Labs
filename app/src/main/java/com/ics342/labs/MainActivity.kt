742ca3e1ca84aa5140b3fd5aae9dca8489d56a7a
Yeliz Konduk <yelizkonduk@gmail.com>
2023-07-31 03:24:55 -0500

    Implemented notification feature in MainActivity and NotificationService

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -1,3 +1,139 @@
+// The application's main activity is responsible for requesting
+// the POST_NOTIFICATIONS permission from the user, showing a rationale dialog if needed,
+// and displaying a message when the permission is granted.
+// Resources:
+//https://developer.android.com/develop/ui/views/notifications/build-notification
+
+package com.ics342.labs
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import com.ics342.labs.ui.theme.LabsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Define mutable state variables to track
+        // permission status and whether to show rationale dialog
+        setContent {
+            var hasPermission by remember { mutableStateOf(false) } // Permission granted status
+            var showPermissionRationale by remember { mutableStateOf(false) } // Rationale dialog status
+            val context = LocalContext.current    //Retrieves the current context
+
+            // Defines a launcher for a permission request.
+            val launcher = rememberLauncherForActivityResult(
+                RequestPermission()
+            ) { isGranted ->
+                if (isGranted) { // If permission is granted, update the state
+                    hasPermission = true
+                } else {      // Check if rationale dialog should be shown
+                    showPermissionRationale = shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+            // Define the app theme and layout
+            LabsTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        if (hasPermission) {
+                            LocationView()   // Show view if permission is granted
+                        } else {
+                            Button(onClick = {
+                                // Check or request permission on button click
+                                checkOrRequestPermission(
+                                    context,
+                                    Manifest.permission.POST_NOTIFICATIONS,
+                                    launcher
+                                ) { hasPermission = true }
+                            }) {
+                                Text("Enable Notifications")
+                            }
+                        }
+                        // Show rationale dialog if required
+                        if (showPermissionRationale) {
+                            PermissionRationaleDialog(
+                                onConfirm = { launcher.launch(Manifest.permission.POST_NOTIFICATIONS) },
+                                onCancel = { showPermissionRationale = false }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+// View for displaying a text when permission is granted
+@Composable
+fun LocationView() {
+    Text("You have enabled notifications. Thank you!")
+}
+// Rationale dialog component
+@Composable
+fun PermissionRationaleDialog(
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = {
+            Button(onClick = { onConfirm() }) {
+                Text("Yes, Allow")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { onCancel() }) {
+                Text("No, Thanks")
+            }
+        },
+        title = {
+            Text("Enable Notifications?")
+        },
+        text = {
+            Text("Would you like to enable notifications for our app? This allows you to stay updated with our latest content.")
+        }
+    )
+}
+// Function to check or request a permission
+private fun checkOrRequestPermission(
+    context: Context,
+    permission: String,
+    launcher: ManagedActivityResultLauncher<String, Boolean>,
+    permissionGranted: () -> Unit
+) {
+    when (PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(context, permission) -> {
+            permissionGranted()  // Call callback if permission is already granted
+        }
+        else -> {
+            launcher.launch(permission)  // Request permission if not granted
+        }
+    }
+}
+/*// Lab 8: Notifications
+
 package com.ics342.labs
 
 import android.Manifest
@@ -62,8 +198,10 @@ class MainActivity : ComponentActivity() {
 }
 
 fun startNotificationService(context: Context) {
-    TODO("Start the Notification Service")
+    val intent = Intent(context, NotificationService::class.java)
+    ContextCompat.startForegroundService(context, intent)
 }
+
 
 private fun checkOrRequestPermission(
     context: Context,
@@ -79,4 +217,4 @@ private fun checkOrRequestPermission(
     } else {
         launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
     }
-}
+}*/
--- app/src/main/java/com/ics342/labs/NotificationService.kt
+++ app/src/main/java/com/ics342/labs/NotificationService.kt
@@ -1,56 +1,118 @@
+// The application's main activity is responsible for requesting
+// the POST_NOTIFICATIONS permission from the user, showing a rationale dialog if needed,
+// and displaying a message when the permission is granted.
+// Resources:
+//https://developer.android.com/develop/ui/views/notifications/build-notification
+
 package com.ics342.labs
 
-import android.Manifest.permission
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+// Define a NotificationService class that extends Android Service
+class NotificationService : Service() {
+    // onBind method should be implemented for a Service,
+    // but in this case, it returns null, as binding is not used
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+    // onStartCommand is called when the service is started.
+    // It's responsible for displaying the notification
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Create a notification using NotificationCompat
+        // Builder, specifying the channel, title, text, icon, and priority
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("New Content Available!")
+            .setContentText("Check out the latest updates in Lab 8.")
+            .setSmallIcon(R.drawable.star)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        // Retrieve the NotificationManager system service, responsible for issuing notifications
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // Notify the NotificationManager to display the notification, using the defined notification ID
+        notificationManager.notify(NOTIFICATION_ID, notification)
+        // Return the constant from the parent onStartCommand,
+        // -dictates how the system should proceed with the service if it's killed
+        return super.onStartCommand(intent, flags, startId)
+    }
+    // Define constants for the channel ID and notification ID within a companion object,
+    // can be referenced statically
+    companion object {
+        const val CHANNEL_ID = "LAB_8_CHANNEL_ID"
+        const val NOTIFICATION_ID =  1234
+    }
+}
+
+/*package com.ics342.labs
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import android.app.Service
+import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
-import java.util.UUID
 
-class NotificationService: Service() {
+class NotificationService : Service() {
 
-    private val notificationManager: NotificationManagerCompat =
-        NotificationManagerCompat.from(this)
+    private lateinit var notificationManager: NotificationManagerCompat
+
 
     override fun onCreate() {
         super.onCreate()
+        notificationManager = NotificationManagerCompat.from(this)
         createNotificationChannel()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // If permission has not been granted, stop the service and return from
-        // onStartCommand
+
         if (ContextCompat.checkSelfPermission(
                 this@NotificationService,
-                permission.POST_NOTIFICATIONS
+                Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             stopSelf()
             return START_NOT_STICKY
         }
 
-       // Build notification
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.star)
+            .setContentTitle("Lab 8 Notification")
+            .setContentText("This is Lab 8 notification.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
 
-        TODO("Build and show notification")
+        startForeground(NOTIFICATION_ID, notification)
         return START_STICKY_COMPATIBILITY
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        // No need to implement for lab 8
         return null
     }
 
     private fun createNotificationChannel() {
-        TODO("Create notification channel and register with the system")
+        val name = "Lab 8 Channel"
+        val descriptionText = "Channel for Lab 8 notifications"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
+
     companion object {
-        private const val CHANNEL_ID = "LAB_7_CHANNEL_ID"
+        private const val CHANNEL_ID = "LAB_8_CHANNEL_ID"
         private const val NOTIFICATION_ID = 1234
     }
-}
+}*/
